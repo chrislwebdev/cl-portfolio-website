4dc7f2bf84d7b357c0df1c9ce1fa3cd03f7ecf78
chrislwebdev <chrislwebdev@gmail.com>
2023-11-20 14:16:07 +0300

    Added images and projects

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+const navBtn = document.querySelector(".nav-buttons-container");
+const navOpen = document.querySelector(".nav-open-btn");
+const navClose = document.querySelector(".nav-close-btn");
+const navbar = document.querySelector(".hero-navbar");
 
-const navBtn = document.querySelector('.nav-buttons-container')
-const navOpen = document.querySelector('.nav-open-btn');
-const navClose = document.querySelector('.nav-close-btn')
-const navbar = document.querySelector('.hero-navbar');
+navBtn.addEventListener("click", () => {
+  navClose.classList.toggle("visible");
+  navOpen.classList.toggle("invisible");
+  navbar.classList.toggle("visible");
+});
 
-navBtn.addEventListener('click', () => {
-    navClose.classList.toggle('visible');
-    navOpen.classList.toggle('invisible');
-    navbar.classList.toggle('visible')
-})
+const removeBodyLoad = () => {
+  if (sessionStorage.getItem("load")) {
+    const loading = document.querySelector(".loading");
+    loading.remove();
+    return;
+  }
+  setTimeout(() => {
+    const loading = document.querySelector(".loading");
+    loading.remove();
+  }, 1500);
+  sessionStorage.setItem("load", "load");
+};
+
+removeBodyLoad();
